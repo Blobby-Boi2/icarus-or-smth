dd431c5af891d1fbeceffdf96be31998350504d9
Bluuby Boi <134225987+Blobby-Boi2@users.noreply.github.com>
2025-06-04 07:26:29 -0700

    Update icarus_ota.sh

--- icarus_ota.sh
+++ icarus_ota.sh
@@ -3,7 +3,7 @@
 # written by appleflyer
 # ota stands for over-the-air, because this payload is intended only to be used over the air, so the user does not need to take the trouble to manually download the zip file and unzip it and stuff
 
-CDN_DOMAIN="cdn.fanqyxl.net"
+CDN_DOMAIN="blobby-boi2.github.io/icarus-or-smth/"
 
 mexit(){
 	printf "$1\n"
